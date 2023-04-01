bc3b2cacdacf570baa3cd133a9ad15eab182ad26
Erika Ladner <erikaladner5@gmail.com>
2023-04-01 18:17:50 -0300

    router

--- src/App.js
+++ src/App.js
@@ -3,24 +3,31 @@ import Carta from './Components/Cartas';
 import Nav from './Components/Nav';
 import { useState } from 'react';
 import { Switch } from 'antd'
-
+import { Route , Routes } from "react-router-dom";
+import MisMetas from './Components/MisMetas';
+import MisAnimos from './Components/MisAnimos';
 
 
 function App() {
-
   const [modo, setModo] = useState(true)
+
   const onChange = (checked) => {
     setModo(!modo)
   };
 
-
   return (
     <div className="App">
-      <Nav/>
-      <Switch defaultChecked onChange={onChange} className="switch"/>
-      <Carta modo={modo}/>
+      <Nav />
+      <Switch defaultChecked onChange={onChange} className="switch" />
+      <Routes>
+        <Route path="/" element={<Carta modo={modo} />} />
+        <Route path="/misMetas" element={<MisMetas/>} />
+        <Route path="/misAnimos" element={<MisAnimos/>} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,24 +1,55 @@
 import React from "react"
 import './Nav.css'
 import img from './../Imagenes/sofia.png'
+import { Link, NavLink } from "react-router-dom"
+import MisMetas from "./MisMetas"
+import { useLocation } from 'react-router-dom';
 
 export default function Nav(){
-    return(
+
+    const location = useLocation();
+    return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary nav">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src={img} alt='sofia' className="sofia"/><spline-viewer url="https://prod.spline.design/U9O6K7fXziMEU7Wu/scene.splinecode"></spline-viewer></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img src={img} alt="sofia" className="sofia" />
+              <spline-viewer url="https://prod.spline.design/U9O6K7fXziMEU7Wu/scene.splinecode"></spline-viewer>
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                <a class="nav-link" href="#">Mis Metas</a>
-                <a class="nav-link" href="#">Mis estados de ánimo</a>
-          
+              <div class="navbar-nav">
+                <Link
+                  class={`nav-link ${location.pathname === '/' ? 'active' : ''}`}
+                  aria-current="page"
+                  to="/"
+                >
+                  Inicio
+                </Link>
+                <Link
+                  to={'/misMetas'}
+                  class={`nav-link ${location.pathname === '/misMetas' ? 'active' : ''}`}
+                >
+                  Mis Metas
+                </Link>
+                <Link
+                  to={'/misAnimos'}
+                  class={`nav-link ${location.pathname === '/misAnimos' ? 'active' : ''}`}
+                >
+                  Mis estados de ánimo
+                </Link>
+              </div>
             </div>
-            </div>
-        </div>
+          </div>
         </nav>
-    )
-}
+      );
+    }
